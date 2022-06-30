41d8804ba40290220eb023216051ff08f1bdc61b
import023 <0_2_3@naver.com>
2022-07-01 00:51:07 +0900

    220701

--- MOMENTUM/js/app.js
+++ MOMENTUM/js/app.js
@@ -1,53 +1,28 @@
-// const h1 = document.querySelector("div.hello:first-child h1");
-
-// function handlerTitleMouseEnter() {
-//     const clickClass = "clicked"
-    // if(h1.classList.contains(clickClass)){
-    //     h1.classList.remove(clickClass)
-    // } else {
-    //     h1.classList.add(clickClass)
-    // }
-//     h1.classList.toggle("clicked")
-// }
-
-//h1.addEventListener("click", handlerTitleMouseEnter)
-
-const loginForm = document.querySelector("#login-form")
-const loginInput = loginForm.querySelector("#login-form input")
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden"
-const USERNAME_KEY = "username"
-const link = document.querySelector("a")
-const savedUserName = localStorage.getItem(USERNAME_KEY);
-
-function paintGreeting(){
-    greeting.innerText = `Hello ${savedUserName}`
-    greeting.classList.remove(HIDDEN_CLASSNAME)
-}
-
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    const userName = loginInput.value
-    event.preventDefault()
-    console.log(userName)
-    loginForm.classList.add(HIDDEN_CLASSNAME)
-    console.dir(greeting)
-    greeting.classList.remove(HIDDEN_CLASSNAME)
-    //greeting.innerText = "Hello, " + userName
-    paintGreeting()
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-// function handlerLink(event){
-//     event.preventDefault()
-//     console.dir(event)
-// }
-// link.addEventListener("click", handlerLink)
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if (savedUserName == null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME)
-    loginForm.addEventListener("submit",onLoginSubmit)    
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreeting()
+  paintGreetings(savedUsername);
 }
--- MOMENTUM/js/todo.js
+++ MOMENTUM/js/todo.js
@@ -1,15 +1,36 @@
 const toDoForm = document.getElementById("todo-form")
 const toDoList = document.getElementById("todo-list")
 const toDoInput = toDoForm.querySelector("#todo-form input")
+const TODOS_KEY = "todos"
+let toDos = []
 
-function paintToDo(newToDo){
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+}
+
+function deleteToDo(event){
+    console.log(event)
+    const li = (event.target.parentElement)
+    //toDos.remove(id : li.id)
+    toDos.pop(`id:${li.id}`)
+    localStorage.clear()
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+    li.remove()
+    //localStorage.removeItem(li.innerText)
+}
+
+function paintToDo(newToDoObj){
     const liCreator = document.createElement("li");
     const spanCreator = document.createElement("span");
+    const buttonCreator = document.createElement("button")
     liCreator.appendChild(spanCreator)
-    spanCreator.innerText = newToDo
+    liCreator.appendChild(buttonCreator)
+    spanCreator.innerText = newToDoObj.text
+    buttonCreator.innerText = "❌"
+    buttonCreator.addEventListener("click",deleteToDo)
+    liCreator.id = newToDoObj.id
     toDoList.appendChild(liCreator)
     // liCreator.innerText(BindNewToDo)
-    console.log(toDoList)
     
 }
 
@@ -17,7 +38,29 @@ function handleToDoSubmit(event){
     event.preventDefault()   
     const newToDo = toDoInput.value
     toDoInput.value = ""
-    paintToDo(newToDo)
+    const newToDoObj = {
+        text : newToDo,
+        id : Date.now(),
+    }
+    toDos.push(newToDoObj)
+    paintToDo(newToDoObj)
+    saveToDos()
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit)
+
+//instead of "(item) => toDos.push(item))"
+// function sayHello(item){
+//     toDos.push(item)
+// }
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if(savedToDos != null){
+    const parsedToDo = JSON.parse(savedToDos);
+    parsedToDo.forEach((item) => toDos.push(item));
+    toDos = parsedToDo;
+    parsedToDo.forEach(paintToDo)//parsedToDo.forEach((item) => paintToDo(item))//instead of "parsedToDo.forEach(toDos"
+
+}
+
