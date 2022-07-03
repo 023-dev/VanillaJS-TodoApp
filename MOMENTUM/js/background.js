cc97aef557cd0f8f4266f1824872d0483d555c31
import023 <0_2_3@naver.com>
2022-07-03 23:43:56 +0900

    220703

--- MOMENTUM/js/background.js
+++ MOMENTUM/js/background.js
@@ -11,7 +11,10 @@
 const imageNames = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"]
 const chosenImage = imageNames[Math.floor(Math.random() * (imageNames.length-1))]
 const bgImage = document.createElement("img")
-
-bgImage.src = `image/${chosenImage}`
+const bgImageUrl = `image/${chosenImage}`
+document.body.style.backgroundImage = `url('${bgImageUrl}')`
+//bgImage.src = `image/${chosenImage}`
 bgImage.style.backgroundSize = "cover"
 document.body.appendChild(bgImage)
+
+
