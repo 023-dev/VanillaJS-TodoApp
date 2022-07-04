f9e315330f551130b7d139c5476a137bfe66fd75
import023 <0_2_3@naver.com>
2022-07-04 16:06:04 +0900

    220704

---
+++ js/clock.js
@@ -0,0 +1,15 @@
+const clock = document.querySelector("h2#clock")
+
+function getClock(){
+    const date = new Date();
+    const hours = `${date.getHours()}`.padStart(2,"0");
+    const minutes = `${date.getMinutes()}`.padStart(2,"0");
+    const seconds = `${date.getSeconds()}`.padStart(2,"0");
+    // const hour = new Hour()
+    // const minute = new Min
+
+    clock.innerText = `${hours}:${minutes}:${seconds}`
+}
+getClock()
+setInterval(getClock, 1000)
+
